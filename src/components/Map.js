e4b6ea99d79e900a4f833a88def0c886e7a6ff28
kurves <84717663+kurves@users.noreply.github.com>
2021-12-30 17:15:23 +0300

    developing Map.js

---
+++ src/components/Map.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {  MapContainer,TileLayer,Marker,Popup } from 'react-leaflet'
+import './Map.css';
+import {showMapData} from '../utilities/utils';
+
+function Map({center,zoom,countries,casesType}) {
+    
+    return (
+			<div className="map">
+				<MapContainer  center={center} zoom={zoom}>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					{showMapData(countries,casesType)}
+				</MapContainer>
+			</div>
+		);
+}
+
+export default Map
+
